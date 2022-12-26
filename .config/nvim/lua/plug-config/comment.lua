d4e135bc5eabe7ee3348683494ded3fd92d75abd
tiberlas <tiberlas1997@gmail.com>
2022-12-26 15:31:40 +0100

    [nvim] fix shortcut for comment

--- .config/nvim/lua/plug-config/comment.lua
+++ .config/nvim/lua/plug-config/comment.lua
@@ -6,8 +6,8 @@ end
 
 comment.use_extended_mappings()
 
-KEYMAP("n", "<leader>ci", "<Plug>kommentary_line_increase", OPTS)
-KEYMAP("x", "<leader>ci", "<Plug>kommentary_visual_increase", OPTS)
+KEYMAP("n", "<leader>cc", "<Plug>kommentary_line_increase", OPTS)
+KEYMAP("x", "<leader>cc", "<Plug>kommentary_visual_increase", OPTS)
 KEYMAP("n", "<leader>cd", "<Plug>kommentary_line_decrease", OPTS)
 KEYMAP("x", "<leader>cd", "<Plug>kommentary_visual_decrease", OPTS)
 
